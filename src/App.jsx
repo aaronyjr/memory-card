cabbb3db3fca4e70fd5679be5dae83920fd4b94a
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-09-21 12:30:13 +0800

    Add instructions

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import "./App.css";
 import { LeagueCards } from "./components/LeagueCards";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  if (score > highScore) {
+    setHighScore(highScore + 1);
+  }
+
   return (
     <div>
+      <h1>Memory Game</h1>
+      <p>
+        Get points by clicking on an image, but don't click on an image more
+        than once!
+      </p>
+      <div>
+        <p>Score: {score}</p>
+        <p>High score: {highScore}</p>
+      </div>
       <LeagueCards />
     </div>
   );
 }
 
 export default App;
-
--- src/components/LeagueCards.jsx
+++ src/components/LeagueCards.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { leagueChampions } from "../data/Champions";
 import ReactParallaxTilt from "react-parallax-tilt";
-import '../styles/CardStyle.css'
+import "../styles/CardStyle.css";
 
 const BASE_URL =
   "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/";
@@ -42,42 +42,41 @@ export function LeagueCards() {
       {championCards.length > 0 ? (
         <div
           style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(4, 1fr)',
-            gap: '2px',
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gap: "2px",
           }}
         >
           {championCards.map((card, index) => (
             <ReactParallaxTilt key={index}>
               <div
+                onClick={() => console.log("Clicked")}
                 className="card"
                 style={{
-                  position: 'relative', // Added relative positioning
-                  textAlign: 'center',
+                  position: "relative",
+                  textAlign: "center",
                 }}
               >
-                {/* Image */}
                 <img
                   src={card.imageUrl}
                   alt={`${card.name} card`}
                   style={{
-                    width: '80%',
-                    maxHeight: '380px',
+                    width: "80%",
+                    maxHeight: "380px",
                   }}
                 />
-  
-                {/* Title */}
+
                 <h3
                   style={{
-                    position: 'absolute',
-                    bottom: '10px',
-                    left: '50%',
-                    transform: 'translateX(-50%)',
-                    margin: '0',
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: 'white',
-                    padding: '5px',
-                    borderRadius: '8px',
+                    position: "absolute",
+                    bottom: "10px",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    margin: "0",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    color: "white",
+                    padding: "5px",
+                    borderRadius: "8px",
                   }}
                 >
                   {card.name}
@@ -91,8 +90,6 @@ export function LeagueCards() {
       )}
     </>
   );
-  
-  
 }
 
 function generateTenChampionNames() {
